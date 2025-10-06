3e8ec2232c4cb25eebd75d08d27c783e1d62695e
asparagoose <malvrez0525@gmail.com>
2025-10-06 01:41:09 -0700

    I think I forgot to push half my changes xd

---
+++ number_spiral/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+g++ solution.cpp -o solve
+./solve < inputs/1.txt > outputs/1.txt
+./solve < inputs/2.txt > outputs/2.txt
+diff -y outputs/1.txt their_outputs/1.txt > diffs/1.txt
+diff -y outputs/2.txt their_outputs/2.txt > diffs/2.txt
