7a866d7e3bff948c8a92fde2b165fbb2a0e31f9c
asparagoose <malvrez0525@gmail.com>
2025-10-23 19:31:18 -0700

    Finished coin_piles. Not sure why the math was getting me in hindsight.

--- introductory_problems/coin_piles/solution.cpp
+++ introductory_problems/coin_piles/solution.cpp
@@ -4,11 +4,12 @@ using ll = long long;
 
 int main(void){
     // math is obliterating me. revisit later.
+    // was basic algebra. not sure why i struggled so hard on it.
     ll t; std::cin >> t;
     ll x, y;
     while(t--){
         std::cin >> x >> y;
-        if((x+y)%3) std::cout << "NO" << std::endl;
+        if( (((2*y)-x) < 0) || (((2*y)-x)%3) || (((2*x)-y) < 0) || (((2*x)-y)%3)) std::cout << "NO" << std::endl;
         else std::cout << "YES" << std::endl;
     }
 
